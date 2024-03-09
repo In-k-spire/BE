96bb4f45f7b1afffb760a583af34824a18b0a325
soohyang136 <jsh515228@gmail.com>
2024-03-10 02:14:17 +0900

    init

---
+++ src/main/kotlin/suhyang/reAct/ReActApplication.kt
@@ -0,0 +1,11 @@
+package suhyang.reAct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReActApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReActApplication>(*args)
+}
---
+++ src/test/kotlin/suhyang/reAct/ReActApplicationTests.kt
@@ -0,0 +1,13 @@
+package suhyang.reAct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReActApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
