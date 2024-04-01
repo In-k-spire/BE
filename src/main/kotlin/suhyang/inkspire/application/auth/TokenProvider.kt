06cd859483fc3ee3cca999d79b2ca5207c2db7d0
soohyang136 <jsh515228@gmail.com>
2024-04-01 23:38:33 +0900

    CHORE :: resolve error

--- src/main/kotlin/suhyang/inkspire/application/auth/TokenProvider.kt
+++ src/main/kotlin/suhyang/inkspire/application/auth/TokenProvider.kt
@@ -1,7 +1,7 @@
 package suhyang.inkspire.application.auth
 
 interface TokenProvider {
-    fun createAccessToken(payload: String): String;
-    fun createRefreshToken(payload: String): String;
+    fun createAccessToken(subject: String): String;
+    fun createRefreshToken(subject: String): String;
     fun getSubject(token: String): String;
 }
