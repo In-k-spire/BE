e0ea3ec43572f49c6ce2099056039c3f8886046a
soohyang136 <jsh515228@gmail.com>
2024-10-02 00:40:13 +0900

    chore:: EnableJpaAuditing confingration으로 빼기

--- src/main/kotlin/suhyang/inkspire/InkspireApplication.kt
+++ src/main/kotlin/suhyang/inkspire/InkspireApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class InkspireApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/suhyang/inkspire/common/config/JpaAuditingConfiguration.kt
@@ -0,0 +1,9 @@
+package suhyang.inkspire.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfiguration {
+}
